8ebe5901a7645f71cf5b89d9f70160eca2e3c2f9
khurshid <rakhmatullaevkhurshid17@gmail.com>
2021-01-11 18:30:46 +0500

    script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+var name = prompt('Введите своё имя');
+console.log("Меня зовут " + name);
+var age = prompt('Введите свой возрост')
+console.log("Мне " + age);
+var example = prompt('Решите пример 6 + 3 = ?')
+console.log("Пример 1: " + "6 + 3 = 9 " + "Ваш ответ " + example);
+var example_2 = prompt('Решите пример 20 - 5 = ?')
+console.log("Пример 2: " + "20 - 5 = 15 " + "Ваш ответ " + example_2);
+var example_3 = prompt('Решите пример 15 * 2 = ?')
+console.log("Пример 3: " + "15 * 2 = 30 " + "Ваш ответ " + example_3);
+var example_4 = prompt('Решите пример 8 / 4 = ?')
+console.log("Пример 4: " + "8 / 4 = 2 " + "Ваш ответ " + example_4);
+var example_5 = prompt('Решите пример 10 % 3 = ?')
+console.log("Пример 5: " + "10 % 3 = 1 " + "Ваш ответ " + example_5);
+alert("Откройте консоль для просмотра")
